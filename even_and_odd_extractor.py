bcad48bad53d2ae85dde83c84adf541fd195a075
Dale Menciro <dale_menciro@protonmail.com>
2023-04-24 19:26:10 +0800

    deleting the program instruction

--- even_and_odd_extractor.py
+++ even_and_odd_extractor.py
@@ -1,8 +1,3 @@
-#Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
-#The program will create two other text file; the first text file will be named even.txt that 
-#will contains all even numbers extracted from the numbers.txt. The second text file will be named 
-#odd.txt that will contains all odd numbers extracted from the numbers.txt.
-
 #Open 'number.txt' file for read
 with open('numbers.txt') as my_file:
     #read lines and conver it to int
