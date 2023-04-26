15aeba1df0c432ea9afbb8eeae579bf26b1ea4d8
Dale Menciro <dale_menciro@protonmail.com>
2023-04-26 19:11:21 +0800

    messagebox for completion

--- even_and_odd_extractor.py
+++ even_and_odd_extractor.py
@@ -12,24 +12,27 @@ def select_output_dir():
     output_dir = filedialog.askdirectory(title="Select Output Directory")
 
 def extract_numbers():
-    #Open 'number.txt' file for read
-    with open('numbers.txt') as my_file:
-        #read lines and conver it to int
-        numbers= [int(line.strip()) for line in my_file.readlines()]
+    try:
+        #Open 'number.txt' file for read
+        with open('numbers.txt') as my_file:
+            #read lines and conver it to int
+            numbers= [int(line.strip()) for line in my_file.readlines()]
 
-    #open 'even.txt' file to write
-    with open('even.txt', 'w') as file_even:
-        #open 'odd.txt' file to write
-        with open('odd.txt', 'w') as file_odd:
-            #loop each numbers on the list
-            for num in numbers:
-                #check if even
-                if num % 2 == 0:
-                    #write to 'even.txt' if the number is even
-                    file_even.write(str(num) + '\n')
-                else:
-                    #write to 'odd.txt' if the number is odd
-                    file_odd.write(str(num) + '\n')
+        #open 'even.txt' file to write
+        with open('even.txt', 'w') as file_even:
+            #open 'odd.txt' file to write
+            with open('odd.txt', 'w') as file_odd:
+                #loop each numbers on the list
+                for num in numbers:
+                    #check if even
+                    if num % 2 == 0:
+                        #write to 'even.txt' if the number is even
+                        file_even.write(str(num) + '\n')
+                    else:
+                        #write to 'odd.txt' if the number is odd
+                        file_odd.write(str(num) + '\n')
+        messagebox.showinfo("Task Complete", "Even and odd numbers extracted successfully!")
+    except:
 
 # Create the GUI window
 root = tk.Tk()
