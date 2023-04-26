bcf7078fff51adbca6ce7b52d182dbd2a9e0dd0d
Dale Menciro <dale_menciro@protonmail.com>
2023-04-26 19:10:02 +0800

    making the extract lines to def functions

--- even_and_odd_extractor.py
+++ even_and_odd_extractor.py
@@ -11,24 +11,25 @@ def select_output_dir():
     global output_dir
     output_dir = filedialog.askdirectory(title="Select Output Directory")
 
-#Open 'number.txt' file for read
-with open('numbers.txt') as my_file:
-    #read lines and conver it to int
-    numbers= [int(line.strip()) for line in my_file.readlines()]
+def extract_numbers():
+    #Open 'number.txt' file for read
+    with open('numbers.txt') as my_file:
+        #read lines and conver it to int
+        numbers= [int(line.strip()) for line in my_file.readlines()]
 
-#open 'even.txt' file to write
-with open('even.txt', 'w') as file_even:
-    #open 'odd.txt' file to write
-    with open('odd.txt', 'w') as file_odd:
-        #loop each numbers on the list
-        for num in numbers:
-            #check if even
-            if num % 2 == 0:
-                #write to 'even.txt' if the number is even
-                file_even.write(str(num) + '\n')
-            else:
-                #write to 'odd.txt' if the number is odd
-                file_odd.write(str(num) + '\n')
+    #open 'even.txt' file to write
+    with open('even.txt', 'w') as file_even:
+        #open 'odd.txt' file to write
+        with open('odd.txt', 'w') as file_odd:
+            #loop each numbers on the list
+            for num in numbers:
+                #check if even
+                if num % 2 == 0:
+                    #write to 'even.txt' if the number is even
+                    file_even.write(str(num) + '\n')
+                else:
+                    #write to 'odd.txt' if the number is odd
+                    file_odd.write(str(num) + '\n')
 
 # Create the GUI window
 root = tk.Tk()
